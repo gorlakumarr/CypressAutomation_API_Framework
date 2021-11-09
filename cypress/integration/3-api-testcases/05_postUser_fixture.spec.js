fb2250529089bbee2e8b70301963aac0aa421938
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-09 09:26:34 +0530

    Added POST API Code

--- cypress/integration/3-api-testcases/05_postUser_fixture.spec.js
+++ cypress/integration/3-api-testcases/05_postUser_fixture.spec.js
@@ -9,32 +9,56 @@ describe('Post API User test', function () {
     let randomText = ''
 
     it('get users test', function () {
+        cy.log('get users test')
+        cy.log('email', randomText + '@gmail.com')
 
         var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
         for (var ii = 0; ii < 15; ii++) {
             randomText += chars[Math.floor(Math.random() * chars.length)];
         }
 
-
-        cy.log('get users test')
-        cy.request({
-            method: 'POST',
-            url: baseUrlPostAPI,
-            headers: {
-                'Authorization': 'Bearer ' + accessToken
-            },
-            body: {
-                "name": dataJson.name,
-                "email": randomText + "@gmail.com",
-                "gender": dataJson.gender,
-                "status": dataJson.status
-            }
-        }).then((res) => {
-            expect(res.status).to.be.equal(201)
-            expect(res.body.data).have.property('id')
-            expect(res.body.data).have.property('name', dataJson.name)
-            expect(res.body.data).have.property('email', randomText + "@gmail.com")
-            cy.log("Response", JSON.stringify(res))
+        // One way
+        cy.fixture('createUser').then((payload) => {
+            cy.request({
+                method: 'POST',
+                url: baseUrlPostAPI,
+                headers: {
+                    'Authorization': 'Bearer ' + accessToken
+                },
+                body: {
+                    "name": payload.name,
+                    "email": randomText + "@gmail.com",
+                    "gender": payload.gender,
+                    "status": payload.status
+                }
+            }).then((res) => {
+                expect(res.status).to.be.equal(201)
+                expect(res.body.data).have.property('id')
+                expect(res.body.data).have.property('name', payload.name)
+                expect(res.body.data).have.property('email', randomText + "@gmail.com")
+                cy.log("Response", JSON.stringify(res))
+            })
         })
+
+        // Another way
+        // cy.request({
+        //     method: 'POST',
+        //     url: baseUrlPostAPI,
+        //     headers: {
+        //         'Authorization': 'Bearer ' + accessToken
+        //     },
+        //     body: {
+        //         "name": dataJson.name,
+        //         "email": randomText + "@gmail.com",
+        //         "gender": dataJson.gender,
+        //         "status": dataJson.status
+        //     }
+        // }).then((res) => {
+        //     expect(res.status).to.be.equal(201)
+        //     expect(res.body.data).have.property('id')
+        //     expect(res.body.data).have.property('name', dataJson.name)
+        //     expect(res.body.data).have.property('email', randomText + "@gmail.com")
+        //     cy.log("Response", JSON.stringify(res))
+        // })
     })
 })
