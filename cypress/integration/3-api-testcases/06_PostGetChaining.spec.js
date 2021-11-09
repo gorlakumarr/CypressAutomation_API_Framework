b5e9530528d306129001b79d13fcbe7871c105c7
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-09 09:35:54 +0530

    Added Post with Get Code to Check the Id

---
+++ cypress/integration/3-api-testcases/06_PostGetChaining.spec.js
@@ -0,0 +1,58 @@
+/// <reference types="Cypress" />
+
+const dataJson = require('../../fixtures/createUser.json')
+
+describe('Post API User test', function () {
+
+    let accessToken = '2de746467799ecef3d6ae3ed750f243d58c59594346b6983450aff3141c44bdd'
+    let baseUrlPostAPI = 'https://gorest.co.in/public/v1/users'
+    let randomText = ''
+
+    it('get users test', function () {
+        cy.log('get users test')
+        cy.log('email', randomText + '@gmail.com')
+
+        var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+        for (var ii = 0; ii < 15; ii++) {
+            randomText += chars[Math.floor(Math.random() * chars.length)];
+        }
+
+        // One way
+        cy.fixture('createUser').then((payload) => {
+            cy.request({
+                method: 'POST',
+                url: baseUrlPostAPI,
+                headers: {
+                    'Authorization': 'Bearer ' + accessToken
+                },
+                body: {
+                    "name": payload.name,
+                    "email": randomText + "@gmail.com",
+                    "gender": payload.gender,
+                    "status": payload.status
+                }
+            }).then((res) => {
+                expect(res.status).to.be.equal(201)
+                expect(res.body.data).have.property('id')
+                expect(res.body.data).have.property('name', payload.name)
+                expect(res.body.data).have.property('email', randomText + "@gmail.com")
+                cy.log("Response", JSON.stringify(res))
+            })
+                .then((response) => {
+                    const userId = response.body.data.id
+                    cy.log('userId', userId)
+
+                    cy.request({
+                        method: 'GET',
+                        url: baseUrlPostAPI + '/' + userId,
+                        headers: {
+                            'authorization': 'Bearer ' + accessToken
+                        }
+                    }).then((getRes) => {
+                        expect(getRes.status).to.be.equal(200)
+                        expect(getRes.body.data).have.property('id', userId)
+                    })
+                })
+        })
+    })
+})
