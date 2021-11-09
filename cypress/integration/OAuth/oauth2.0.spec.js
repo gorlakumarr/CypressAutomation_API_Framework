98a1ebf735fe4da059724679e66e7d37a7406f9c
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-09 11:32:24 +0530

    Added OAuth 2.0

---
+++ cypress/integration/OAuth/oauth2.0.spec.js
@@ -0,0 +1,50 @@
+/// <reference types="Cypress" />
+
+describe('OAuth feature test', function () {
+
+    let access_token = ''
+    let baseUrl = 'http://coop.apps.symfonycasts.com'
+    let userId = ''
+
+    before('Generate Token and User Id', () => {
+        cy.request({
+            method: 'POST',
+            url: baseUrl + '/token',
+            form: true,
+            body: {
+                "client_id": 'cypressApplication',
+                "client_secret": '4fb4f73eadf8d0b4764a0c5dddee0c72',
+                "grant_type": 'client_credentials'
+            }
+        }).then((response) => {
+            cy.log('Token Response ', JSON.stringify(response))
+            access_token = response.body.access_token
+            cy.log('token', response.body.access_token)
+
+            cy.request({
+                method: 'GET',
+                url: baseUrl + '/api/me',
+                headers: {
+                    'Authorization': "Bearer " + access_token
+                }
+            }).then((getRes) => {
+                userId = getRes.body.id
+                cy.log('userId', userId)
+            })
+        })
+    })
+
+    it('Unlock Chickens Feed', () => {
+        cy.request({
+            method: 'POST',
+            url: baseUrl + '/api/' + userId + '/chickens-feed',
+            headers: {
+                'Authorization': "Bearer " + access_token
+            }
+        }).then((putRes) => {
+            cy.log('Post Response', JSON.stringify(putRes))
+            expect(putRes.status).to.be.equal(200)
+            expect(putRes.body).have.property('action', 'chickens-feed')
+        })
+    })
+})
